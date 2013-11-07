60e270c55cfbd5e1f3c4ab0d38cfbdebc8ae2403
Brett Anthony <brettanthony@outlook.com>
2013-11-07 22:37:12 +1100

    Added all xcode changes. Caused by an error in the ignore file.

---
+++ NathansWay/obj/Xcode/0/AspyobjClib.h
@@ -0,0 +1,15 @@
+//
+//  AspyobjClib.h
+//  NathansWay.iOS.Numeracy
+//
+//  Created by Brett Anthony on 7/11/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface CustomSlideSegue : UIStoryboardSegue
+
+@end
