16c492c9c3c29676e83b8548a6f0f391f591550e
Brett Anthony <brettanthony@outlook.com>
2013-11-28 13:40:27 +1100

    Continued Nib/View work.

--- NathansWay/NathansWay.Numeracy.iOS/WorkSpace/MVC/QAView.cs
+++ NathansWay/NathansWay.Numeracy.iOS/WorkSpace/MVC/QAView.cs
@@ -7,48 +7,7 @@ using MonoTouch.ObjCRuntime;
 
 namespace NathansWay.WorkSpace
 {
-    public partial class QAView : UIViewController
-    {
-		private vwQAWorkSpace vwQAView;
-
-		public QAView() : base("QAView", null)
-        {
-
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            // Releases the view if it doesn't have a superview.
-            base.DidReceiveMemoryWarning();
-			
-            // Release any cached data, images, etc that aren't in use.
-        }
-
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-
-			vwQAView = new vwQAWorkSpace ();
-			View.AddSubview (vwQAView);
-			
-            // Perform any additional setup after loading the view, typically from a nib.
-        }
-
-		public vwQAWorkSpace xibViewLayer
-		{
-			get { return this.vwQAView; }
-		}
-		public UILabel Q1
-		{
-			get { return this.Q1; }
-		}
-		public UILabel Q2
-		{
-			get { return this.Q2; }
-		}
-    }
-
-		public partial class vwQAWorkSpace : AspyView
+	public partial class vwQAWorkSpace : AspyView
 	{
 
 		public vwQAWorkSpace(IntPtr h): base (h)
@@ -61,9 +20,6 @@ namespace NathansWay.WorkSpace
 			var v = Runtime.GetNSObject(arr.ValueAt(0)) as UIView;
 			v.Frame = UIScreen.MainScreen.Bounds;
 			this.AddSubview(v);
-
-
-
 		}
 
 	}
