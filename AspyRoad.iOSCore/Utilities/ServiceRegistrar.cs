ca56021f068d3a8ded21c6cce846c9fcfb0371cb
Brett Anthony <brettanthony@outlook.com>
2014-04-07 10:49:27 +1000

    Redo the service registrar for better OOP patterns

--- AspyRoad.iOSCore/Utilities/ServiceRegistrar.cs
+++ AspyRoad.iOSCore/Utilities/ServiceRegistrar.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 
 
-namespace NathansWay.Numeracy.Shared
+namespace AspyRoad.iOSCore
 {
     /// <summary>
     /// Class for registering services for the app
---
+++ NathansWay.Numeracy.Shared/Utilities/ServiceRegistrar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace NathansWay.Shared
+{
+    /// <summary>
+    /// Class for registering services for the app
+    /// </summary>
+    public static class ServiceRegistrar 
+    {
+        
+		public static void Startup ()
+		{
+			InitializeShared ();
+		}
+        
+		private static void InitializeShared()
+		{
+			//            ServiceContainer.Register<ILoginService> (() => new SampleLoginService ());
+			//            ServiceContainer.Register<IAssignmentService> (() => new SampleAssignmentService ());
+			//
+			//#if !NETFX_CORE
+			//            //Only do these on iOS or Android
+			//            ServiceContainer.Register<MenuViewModel> ();
+			//            ServiceContainer.Register<AssignmentViewModel>();
+			//            ServiceContainer.Register<DocumentViewModel>();
+			//            ServiceContainer.Register<ExpenseViewModel>();
+			//            ServiceContainer.Register<HistoryViewModel>();
+			//            ServiceContainer.Register<ItemViewModel>();
+			//            ServiceContainer.Register<LaborViewModel>();
+			//            ServiceContainer.Register<LoginViewModel>();
+			//            ServiceContainer.Register<PhotoViewModel>();
+			//#endif		
+		}
+    }
+}
