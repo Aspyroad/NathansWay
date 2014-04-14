de5880170c6db3ad979e65d09ef8f21512c59563
Brett Anthony <brettanthony@outlook.com>
2014-04-14 10:12:44 +1000

    Business layer work - Begin5

---
+++ NathansWay.Numeracy.Shared/DataLayer/NathansWay.Shared.DB.Factory.cs
@@ -0,0 +1,33 @@
+﻿// Core
+using System;
+// SQLite
+using SQLite.Net;
+using SQLite.Net.Interop;
+// Shared
+using NathansWay.Shared;
+using NathansWay.Shared.Global;
+
+
+namespace NathansWay.Shared.DB
+{
+    public class DBFactory
+    {
+
+        #region Class Variables
+
+        protected ISQLitePlatform _SQLitePlatform = SharedServiceContainer.Resolve<ISQLitePlatform>();
+        protected ISharedGlobal _sharedglobal = SharedServiceContainer.Resolve<ISharedGlobal>();
+
+        #endregion
+
+        #region Construction
+
+        public DBFactory(G__Entities _entityName)
+        {
+
+        }
+
+        #endregion
+    }
+}
+
