b5fbd3923f422467d7ec495ea4c5a855661025d1
Brett Anthony <brettanthony@outlook.com>
2014-04-29 08:43:34 +1000

    Combobox control - UIPickerViewDelegate 1

--- NathansWay.Numeracy.iOS/NathansWay.Numeracy.iOS/Controls/vcNumberCombo.cs
+++ NathansWay.Numeracy.iOS/NathansWay.Numeracy.iOS/Controls/vcNumberCombo.cs
@@ -16,8 +16,10 @@ namespace NathansWay.Numeracy.iOS
 
         #region Class Variables
 
-        private PickerDataModel pickerDataModel;
-        private UIPickerViewDelegate _pickerdelegate;
+        //private PickerDataModel pickerDataModel;
+        private PickerDelegate _pickerdelegate;
+        private PickerSource _pickersource;
+        public static List<string> items = new List<string>();
 
         #endregion
 
@@ -42,6 +44,17 @@ namespace NathansWay.Numeracy.iOS
         private void Initialize ()
         {
             this.View.Tag = 190;
+
+            items.Add("0");
+            items.Add("1");
+            items.Add("2");
+            items.Add("3");
+            items.Add("4");
+            items.Add("5");
+            items.Add("6");
+            items.Add("7");
+            items.Add("8");
+            items.Add("9");
         }
 
         #endregion
@@ -61,8 +74,18 @@ namespace NathansWay.Numeracy.iOS
             base.ViewDidLoad();
 
             this._pickerdelegate = new PickerDelegate();
+            this._pickersource = new PickerSource();
+
             this.pkNumberPicker.Delegate = this._pickerdelegate;
-            
+            this.pkNumberPicker.DataSource = this._pickersource;
+
+            // wire up the value change method
+            this._pickerdelegate.psValueChanged += (s, e) =>
+            {
+                this.txtNumber.Text = this._pickerdelegate.SelectedItem;
+            };
+
+            //this.txtNumber.Text = this._pickerdelegate.SelectedItem;
             
 
 
@@ -71,11 +94,12 @@ namespace NathansWay.Numeracy.iOS
 //
 //            this.pkNumberPicker.Center = this.txtNumber.Center;
 //
-//            // wire up the value change method
-//            pickerDataModel.ValueChanged += (s, e) => 
+
+//            this.pkNumberPicker.ValueChanged += (s, e) =>
 //            {
-//                this.txtNumber.Text = pickerDataModel.SelectedItem;
-//            };
+//                this.txtNumber.Text = this._pickerdelegate.SelectedItem;
+//            };  
+
 //
 //            // set our initial selection on the label
 //            this.txtNumber.Text = pickerDataModel.SelectedItem;
@@ -85,51 +109,22 @@ namespace NathansWay.Numeracy.iOS
 
         #endregion
 
-        #region UIPicker
-        // Overridden UIPickerViewModel - Serves as the datasource for the picklist
-        protected class PickerDataModel : UIPickerViewModel 
+        public class PickerDelegate : UIPickerViewDelegate
         {
+
             #region Class Variables
 
-            protected List<string> items = new List<string>();
             protected int selectedIndex = 0;
 
             #endregion
 
             #region Events
 
-            public event EventHandler<EventArgs> ValueChanged;
+            public event EventHandler<EventArgs> psValueChanged;
 
             #endregion
 
-            #region Constructor
-
-            public PickerDataModel () : base ()
-            {
-                // This is a hard coded picker.
-                // Populate the picker with numbers 0 to 9
-                this.items.Add("0");
-                this.items.Add("1");
-                this.items.Add("2");
-                this.items.Add("3");
-                this.items.Add("4");
-                this.items.Add("5");
-                this.items.Add("6");
-                this.items.Add("7");
-                this.items.Add("8");
-                this.items.Add("9");
-
-            }
-
-            #endregion
-
-            #region Public Variables
-
-            public List<string> Items
-            {
-                get { return items; }
-                set { items = value; }
-            }
+            #region Public Members
 
             /// <summary>
             /// The current selected item
@@ -144,15 +139,19 @@ namespace NathansWay.Numeracy.iOS
             #region Overrides
 
             /// <summary>
-            /// Called by the picker to determine how many rows are in a given spinner item
+            /// Called when a row is selected in the spinner
             /// </summary>
-            public override int GetRowsInComponent (UIPickerView picker, int component)
+            public override void Selected (UIPickerView picker, int row, int component)
             {
-                return items.Count;
+                selectedIndex = row;
+                if (psValueChanged != null)
+                {
+                    psValueChanged (this, new EventArgs ());
+                }   
             }
 
             /// <summary>
-            /// called by the picker to get the text for a particular row in a particular 
+            /// Called by the picker to get the text for a particular row in a particular 
             /// spinner item
             /// </summary>
             public override string GetTitle (UIPickerView picker, int row, int component)
@@ -160,59 +159,142 @@ namespace NathansWay.Numeracy.iOS
                 return items[row];
             }
 
+            public override UIView GetView(UIPickerView pickerView, int row, int component, UIView _view)
+            {
+                UILabel lbl = new UILabel(new RectangleF(0, 0, 130f, 40f));
+                lbl.TextColor = UIColor.Blue;
+                lbl.Font = UIFont.SystemFontOfSize(30f);
+                lbl.TextAlignment = UITextAlignment.Center;
+                lbl.Text = items[row];
+                return lbl;
+            }   
+
+            #endregion
+
+        }
+        
+        // Get thisn datasource working and then the code should itterate over each row.        
+        protected class PickerSource : UIPickerViewDataSource
+        {
+
+            /// <summary>
+            /// Called by the picker to determine how many rows are in a given spinner item
+            /// </summary>
+            public override int GetRowsInComponent (UIPickerView picker, int component)
+            {
+                return items.Count;
+            } 
+
             /// <summary>
             /// called by the picker to get the number of spinner items
             /// </summary>
             public override int GetComponentCount (UIPickerView picker)
             {
                 return 1;
-            }
-
-            /// <summary>
-            /// called when a row is selected in the spinner
-            /// </summary>
-            public override void Selected (UIPickerView picker, int row, int component)
-            {
-                selectedIndex = row;
-                if (this.ValueChanged != null)
-                {
-                    this.ValueChanged (this, new EventArgs ());
-                }   
-            }
-            
-            public override UIView GetView(UIPickerView picker, int row, int component, UIView view)
-            {
-                // NOTE: Don't call the base implementation on a Model class
-                // see http://docs.xamarin.com/guides/ios/application_fundamentals/delegates,_protocols,_and_events
-                throw new NotImplementedException();
-            }
-
-            #endregion
-        } 
-
-        protected class PickerDelegate : UIPickerViewDelegate
-        {
-
-            public override UIView GetView(UIPickerView pickerView, int row, int component, UIView _view)
-            {
-                UILabel _lblPicker;
-                // NOTE: Don't call the base implementation on a Model class
-                // see http://docs.xamarin.com/guides/ios/application_fundamentals/delegates,_protocols,_and_events
-                throw new NotImplementedException();
-            }           
-
-        }
-        
-        
-        // Get thisn datasource working and then the code should itterate over each row.        
-        protected class PickerSource : UIPickerViewDataSource
-        {
-            
-            
+            }            
             
         }
+
+
+        #region UIPickerViewModel Implementation
+
+        /// <summary>
+        /// Overridden UIPickerViewModel - Serves as the datasource for the picklist
+        /// </summary>
+//        protected class PickerDataModel : UIPickerViewModel 
+//        {
+//            #region Class Variables
+//
+//            protected int selectedIndex = 0;
+//
+//            #endregion
+//
+//            #region Events
+//
+//            public event EventHandler<EventArgs> ValueChanged;
+//
+//            #endregion
+//
+//            #region Constructor
+//
+//            public PickerDataModel ()
+//            {
+//            }
+//
+//            #endregion
+//
+//            #region Public Variables
+//
+//            public List<string> Items
+//            {
+//                get { return items; }
+//                set { items = value; }
+//            }
+//
+//            /// <summary>
+//            /// The current selected item
+//            /// </summary>
+//            public string SelectedItem
+//            {
+//                get { return items[selectedIndex]; }
+//            }
+//
+//            #endregion
+//
+//            #region Overrides
+//
+//            /// <summary>
+//            /// Called by the picker to determine how many rows are in a given spinner item
+//            /// </summary>
+//            public override int GetRowsInComponent (UIPickerView picker, int component)
+//            {
+//                return items.Count;
+//            }
+//
+//            /// <summary>
+//            /// called by the picker to get the text for a particular row in a particular 
+//            /// spinner item
+//            /// </summary>
+//            public override string GetTitle (UIPickerView picker, int row, int component)
+//            {
+//                return items[row];
+//            }
+//
+//            /// <summary>
+//            /// Called by the picker to get the number of spinner items
+//            /// </summary>
+//            public override int GetComponentCount (UIPickerView picker)
+//            {
+//                return 1;
+//            }
+//
+//            /// <summary>
+//            /// called when a row is selected in the spinner
+//            /// </summary>
+//            public override void Selected (UIPickerView picker, int row, int component)
+//            {
+//                selectedIndex = row;
+//                if (this.ValueChanged != null)
+//                {
+//                    this.ValueChanged (this, new EventArgs ());
+//                }   
+//            }
+//
+//            /// <summary>
+//            /// This is called for ever item in the picker source
+//            /// </summary>
+//            public override UIView GetView(UIPickerView picker, int row, int component, UIView view)
+//            {
+//                // NOTE: Don't call the base implementation on a Model class
+//                // see http://docs.xamarin.com/guides/ios/application_fundamentals/delegates,_protocols,_and_events
+//                throw new NotImplementedException();
+//            }
+//
+//            #endregion
+//        } 
 
         #endregion
+
     }
 }
 
