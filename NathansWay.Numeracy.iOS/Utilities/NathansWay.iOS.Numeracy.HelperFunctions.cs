1e40a33d018c8b355ea94d1769b33104b3e637aa
Brett Anthony <brettanthony@outlook.com>
2014-09-09 08:43:16 +1000

    Helper Function iOS Numeracy

---
+++ NathansWay.Numeracy.iOS/Utilities/NathansWay.iOS.Numeracy.HelperFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace NathansWay.iOS.Numeracy
+{
+	public static class HelperFunctions
+	{
+		public HelperFunctions ()
+		{
+		}
+
+		public static RectangleF StringToRectF(string _strRect)
+		{
+			// x, y, width, height
+			var strArray = _strRect.Split (',');
+			RectangleF tmpRect = 
+				new RectangleF (
+					float.Parse (strArray [0]),
+					float.Parse (strArray [1]),
+					float.Parse (strArray [2]),
+					float.Parse (strArray [3])
+				);
+			return tmpRect;
+
+		}
+
+		public static string RectFToString(RectangleF _rctString)
+		{
+			//Convert a RectF to Text (csv)
+			string tmpStrRect = 
+				_rctString.X.ToString () + ',' +
+				_rctString.Y.ToString () + ',' +
+				_rctString.Width.ToString () + ',' +
+				_rctString.Height.ToString ();
+			return tmpStrRect;
+
+		}
+	}
+}
+
