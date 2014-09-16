54c4b8fcbf895bf19664b43c7d9ad30bdd014fb3
Brett Anthony <brettanthony@outlook.com>
2014-09-16 11:31:39 +1000

    Repair iOS 6.1 segue bug - Start UIPicker class 2

---
+++ NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/Menu/MVC/vcMenuStart.designer.cs
@@ -0,0 +1,98 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace NathansWay.iOS.Numeracy.Menu
+{
+	[Register ("vcMenuStart")]
+	partial class vcMenuStart
+	{
+		[Outlet]
+		AspyRoad.iOSCore.AspyButton btnMenuLesson { get; set; }
+
+		[Outlet]
+		AspyRoad.iOSCore.AspyButton btnMenuStudent { get; set; }
+
+		[Outlet]
+		AspyRoad.iOSCore.AspyButton btnTest { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel NathansWay { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UILabel Numbers { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		NathansWay.iOS.Numeracy.Controls.ButtonSchool School { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		NathansWay.iOS.Numeracy.Controls.ButtonStudent Student { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		NathansWay.iOS.Numeracy.Controls.ButtonTeacher Teacher { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		NathansWay.iOS.Numeracy.Controls.ButtonTools Tools { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		vMenuStart vMenuStart { get; set; }
+
+		[Action ("btnMenuActionLessons:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btnMenuActionLessons (NathansWay.iOS.Numeracy.Controls.ButtonLessons sender);
+
+		[Action ("btnMenuActionStudent:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btnMenuActionStudent (NathansWay.iOS.Numeracy.Controls.ButtonStudent sender);
+
+		[Action ("btnMenuActionToolbox:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btnMenuActionToolbox (NathansWay.iOS.Numeracy.Controls.ButtonTools sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (NathansWay != null) {
+				NathansWay.Dispose ();
+				NathansWay = null;
+			}
+			if (Numbers != null) {
+				Numbers.Dispose ();
+				Numbers = null;
+			}
+			if (School != null) {
+				School.Dispose ();
+				School = null;
+			}
+			if (Student != null) {
+				Student.Dispose ();
+				Student = null;
+			}
+			if (Teacher != null) {
+				Teacher.Dispose ();
+				Teacher = null;
+			}
+			if (Tools != null) {
+				Tools.Dispose ();
+				Tools = null;
+			}
+			if (vMenuStart != null) {
+				vMenuStart.Dispose ();
+				vMenuStart = null;
+			}
+		}
+	}
+}
