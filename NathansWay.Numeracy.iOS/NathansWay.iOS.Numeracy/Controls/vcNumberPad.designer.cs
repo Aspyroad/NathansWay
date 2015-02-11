04f82155640394769408877d5113c0dfb8a8be0d
Brett Anthony <brettanthony@outlook.com>
2015-02-11 11:26:43 +1100

    NumberPad Move to Storyboard

--- NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/Controls/vcNumberPad.designer.cs
+++ NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/Controls/vcNumberPad.designer.cs
@@ -1,14 +1,17 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System;
 using System.CodeDom.Compiler;
 
 namespace NathansWay.iOS.Numeracy.Controls
 {
+	[Register ("vcNumberPad")]
 	partial class vcNumberPad
 	{
 		[Outlet]
@@ -44,95 +47,8 @@ namespace NathansWay.iOS.Numeracy.Controls
 		[Outlet]
 		AspyRoad.iOSCore.AspyButton btnCancel { get; set; }
 
-		[Action ("btn0Touch:")]
-		partial void btn0Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn1Touch:")]
-		partial void btn1Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn2Touch:")]
-		partial void btn2Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn3Touch:")]
-		partial void btn3Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn4Touch:")]
-		partial void btn4Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn5Touch:")]
-		partial void btn5Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn6Touch:")]
-		partial void btn6Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn7Touch:")]
-		partial void btn7Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn8Touch:")]
-		partial void btn8Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btn9Touch:")]
-		partial void btn9Touch (MonoTouch.Foundation.NSObject sender);
-
-		[Action ("btnCancelTouch:")]
-		partial void btnCancelTouch (MonoTouch.Foundation.NSObject sender);
-		
 		void ReleaseDesignerOutlets ()
 		{
-			if (btn0 != null) {
-				btn0.Dispose ();
-				btn0 = null;
-			}
-
-			if (btn1 != null) {
-				btn1.Dispose ();
-				btn1 = null;
-			}
-
-			if (btn2 != null) {
-				btn2.Dispose ();
-				btn2 = null;
-			}
-
-			if (btn3 != null) {
-				btn3.Dispose ();
-				btn3 = null;
-			}
-
-			if (btn4 != null) {
-				btn4.Dispose ();
-				btn4 = null;
-			}
-
-			if (btn5 != null) {
-				btn5.Dispose ();
-				btn5 = null;
-			}
-
-			if (btn6 != null) {
-				btn6.Dispose ();
-				btn6 = null;
-			}
-
-			if (btn7 != null) {
-				btn7.Dispose ();
-				btn7 = null;
-			}
-
-			if (btn8 != null) {
-				btn8.Dispose ();
-				btn8 = null;
-			}
-
-			if (btn9 != null) {
-				btn9.Dispose ();
-				btn9 = null;
-			}
-
-			if (btnCancel != null) {
-				btnCancel.Dispose ();
-				btnCancel = null;
-			}
 		}
 	}
 }
