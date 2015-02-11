a0993547951323e041a7e83af6e472cdf37fc2e6
Brett Anthony <thedood@PC2988.local>
2015-02-12 08:29:35 +1100

    NumberPad Move to Storyboard 3

---
+++ NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/Controls/NathansWay.Numeracy.Input.NumberPad.designer.cs
@@ -0,0 +1,185 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System;
+using System.CodeDom.Compiler;
+
+namespace NathansWay.iOS.Numeracy.Controls
+{
+	[Register ("vcNumberPad")]
+	partial class vcNumberPad
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn0 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn1 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn2 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn3 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn4 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn5 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn6 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn7 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn8 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btn9 { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btnBack { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btnForward { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyButton btnLocked { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		AspyRoad.iOSCore.AspyLabel lblNumberPad { get; set; }
+
+		[Action ("btn0Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn0Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn1Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn1Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn2Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn2Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn3Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn3Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn4Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn4Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn5Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn5Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn6Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn6Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn7Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn7Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn8Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn8Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btn9Touch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btn9Touch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btnBackTouch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btnBackTouch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btnForwardTouch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btnForwardTouch (AspyRoad.iOSCore.AspyButton sender);
+
+		[Action ("btnLockedTouch:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void btnLockedTouch (AspyRoad.iOSCore.AspyButton sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+			if (btn0 != null) {
+				btn0.Dispose ();
+				btn0 = null;
+			}
+			if (btn1 != null) {
+				btn1.Dispose ();
+				btn1 = null;
+			}
+			if (btn2 != null) {
+				btn2.Dispose ();
+				btn2 = null;
+			}
+			if (btn3 != null) {
+				btn3.Dispose ();
+				btn3 = null;
+			}
+			if (btn4 != null) {
+				btn4.Dispose ();
+				btn4 = null;
+			}
+			if (btn5 != null) {
+				btn5.Dispose ();
+				btn5 = null;
+			}
+			if (btn6 != null) {
+				btn6.Dispose ();
+				btn6 = null;
+			}
+			if (btn7 != null) {
+				btn7.Dispose ();
+				btn7 = null;
+			}
+			if (btn8 != null) {
+				btn8.Dispose ();
+				btn8 = null;
+			}
+			if (btn9 != null) {
+				btn9.Dispose ();
+				btn9 = null;
+			}
+			if (btnBack != null) {
+				btnBack.Dispose ();
+				btnBack = null;
+			}
+			if (btnForward != null) {
+				btnForward.Dispose ();
+				btnForward = null;
+			}
+			if (btnLocked != null) {
+				btnLocked.Dispose ();
+				btnLocked = null;
+			}
+			if (lblNumberPad != null) {
+				lblNumberPad.Dispose ();
+				lblNumberPad = null;
+			}
+		}
+	}
+}
