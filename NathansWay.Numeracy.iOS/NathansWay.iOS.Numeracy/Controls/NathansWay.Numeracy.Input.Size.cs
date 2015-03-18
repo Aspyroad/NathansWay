bba0ddc0f5bc3fd04aab1300e3f4abddcdf1b067
Brett Anthony <thedood@PC2988.local>
2015-03-19 08:28:40 +1100

    Start recoding the global size class 4

---
+++ NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/Controls/NathansWay.Numeracy.Input.Size.cs
@@ -0,0 +1,263 @@
+﻿// System
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+// Mono
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreAnimation;
+using MonoTouch.CoreGraphics;
+// Aspyroad
+using AspyRoad.iOSCore;
+using AspyRoad.iOSCore.UISettings;
+// Nathansway
+using NathansWay.iOS.Numeracy.UISettings;
+using NathansWay.Shared;
+
+namespace NathansWay.iOS.Numeracy.Controls
+{
+    public abstract class SizeBase
+    {
+        #region Class Variables
+        // X Horizontal
+        // Y Vertical
+        // Starting point when the control is created 
+        public PointF StartPoint { get; set; }
+        // Parent VC
+        public AspyViewController VcParent { get; set; }
+        // Main Control Frame
+        public RectangleF RectMainFrame { get; set; }
+
+        public vcMainContainer VcMainContainer { get; set; }
+        public iOSNumberDim GlobalNumberSizes { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public SizeBase()
+        {
+            Initialize();
+        }
+
+        #endregion
+
+        #region Private Members
+
+        private void Initialize()
+        {
+            //this._vcmc = iOSCoreServiceContainer.Resolve<vcMainContainer>();
+            //this._globalSizes = new iOSNumberDimensions(_vcParent.DisplaySize);
+        }
+
+        #endregion
+
+        #region Public Members
+
+        public virtual void SetHeightWidth ()
+        {
+        }
+
+        public virtual void SetScale (int _scale)
+        {
+        }
+
+        public virtual void RefreshDisplay ()
+        {
+        }
+
+        #endregion
+    }
+
+    // iOS Dimensions
+    // Heights and widths of the initial number text box.
+    // Most sizes are calculated from these values.
+    public sealed class iOSNumberDim
+    {
+        #region Private Variables
+
+        // Default Normal size
+        private G__NumberDisplaySize _size = G__NumberDisplaySize.Normal;
+        private static readonly iOSNumberDim _instance = new iOSNumberDim();
+
+        #endregion
+
+        #region Constructor
+        // Singleton private
+        private iOSNumberDim ()
+        {            
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public static iOSNumberDim Instance
+        {
+            get { return _instance; }
+        }
+        public static G__NumberDisplaySize Size
+        {
+            set
+            {
+                this._size = value; 
+            }
+        }
+        public static float LabelPickerViewHeight
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return 130.0f;
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return 195.0f;
+                }
+                else // Large
+                {
+                    return 260.0f;
+                }
+            }
+        }
+        public static float LabelPickerViewWidth
+        {
+            get
+            {
+                // Constant
+                return 60.0f;
+                //                if (this._size == G__NumberDisplaySize.Normal)
+                //                {
+                //                    return 60.0f;
+                //                }
+                //                else if (this._size == G__NumberDisplaySize.Medium)
+                //                {
+                //                    return 60.0f;
+                //                }
+                //                else // Large
+                //                {
+                //                    return 60.0f;
+                //                }
+            }
+        }
+        public static float MainNumberHeight
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return 60.0f;
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return 90.0f;
+                }
+                else // Large
+                {
+                    return 120.0f;
+                }
+            }
+        }
+        public static float NumberPickerHeight
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return 180.0f;
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return 180.0f;
+                }
+                else // Large
+                {
+                    return 360.0f;
+                }
+            }
+        }
+        public static float TxtNumberHeight
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return 60.0f;
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return 90.0f;
+                }
+                else // Large
+                {
+                    return 120.0f;
+                }
+            }
+        }
+        public static float UpDownButtonHeight
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return 30.0f;
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return 45.0f;
+                }
+                else // Large
+                {
+                    return 60.0f;
+                }
+            }
+        }
+        public static float GlobalNumberWidth
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return 46.0f;
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return 96.0f;
+                }
+                else // Large
+                {
+                    return 102.0f;
+                }
+            }
+        }
+        public static SizeF LabelPickerViewSize
+        {
+            get
+            {
+                return new SizeF(this.LabelPickerViewWidth, this.LabelPickerViewHeight);
+            }
+        }
+        public static UIFont GlobalNumberFont
+        {
+            get
+            {
+                if (this._size == G__NumberDisplaySize.Normal)
+                {
+                    return UIFont.FromName("Arial", 55.0f);
+                }
+                else if (this._size == G__NumberDisplaySize.Medium)
+                {
+                    return UIFont.FromName("Arial", 77.5f);
+                }
+                else // Large
+                {
+                    return UIFont.FromName("Arial", 110.0f);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
+
--- NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/WorkSpace/MVC/vcWorkSpace.cs
+++ NathansWay.Numeracy.iOS/NathansWay.iOS.Numeracy/WorkSpace/MVC/vcWorkSpace.cs
@@ -12,6 +12,7 @@ using AspyRoad.iOSCore;
 using AspyRoad.iOSCore.UISettings;
 // Nathansway
 using NathansWay.iOS.Numeracy.UISettings;
+using NathansWay.iOS.Numeracy.Controls;
 using NathansWay.Shared;
 
 namespace NathansWay.iOS.Numeracy.WorkSpace
@@ -95,45 +96,11 @@ namespace NathansWay.iOS.Numeracy.WorkSpace
 		#endregion
 	}
 
-    public class WorkSpaceSize
+    public class WorkSpaceSize : SizeBase
     {
         #region Class Variables
         // X Horizontal
         // Y Vertical
-        // Starting point when the control is created
-        public PointF _pStartPoint;
-        // Parent VC
-        private AspyViewController _vcParent;
-        // Number Picker Spinner Control
-        public RectangleF _rectNumberPicker;
-        // Main Control Frame
-        public RectangleF _rectMainNumberFrame;
-        // Text Box Frame
-        public RectangleF _rectTxtNumber;
-        // Up Down Button Frames, usually the same
-        public RectangleF _rectUpButton;
-        public RectangleF _rectDownButton;
-        // Label Frame for the Picker View
-        public RectangleF _rectMainNumberWithPicker;
-        // Full Control height
-        public float _fMainNumberHeight;
-        // Number picker height
-        public float _fNumberPickerHeight;
-        // Text Box Height
-        public float _fTxtNumberHeight;
-        // Width is global to the control
-        public float _fGlobalWidth;
-        // Up Down button height
-        public float _fUpDownButtonHeight;
-
-        // Font Size
-        public UIFont _globalFont;
-        // Label 
-        public SizeF _sLabelPickerViewSize;
-
-
-        public vcMainContainer _vcmc;
-        public iOSNumberDimensions _globalSizes;
 
         #endregion
 
@@ -167,95 +134,7 @@ namespace NathansWay.iOS.Numeracy.WorkSpace
 
         public void SetHeightWidth ()
         {
-            _pStartPoint = _vc.View.Frame.Location;
-            this._sLabelPickerViewSize = 
-                new SizeF(this._globalSizes._sLabelPickerViewWidth, this._globalSizes._sLabelPickerViewHeight);                     
-            // All Initial Values
-            this._fMainNumberHeight = this._globalSizes._fMainNumberHeight;
-            this._fNumberPickerHeight = this._globalSizes._fNumberPickerHeight;
-            this._fTxtNumberHeight = this._globalSizes._fTxtNumberHeight;
-            this._fUpDownButtonHeight = this._globalSizes._fUpDownButtonHeight;
-            // Global width for all heights.
-            this._fGlobalWidth = this._globalSizes._fGlobalWidth;
-            // Font
-            this._globalFont = this._globalSizes._globalFont;
-        }
 
-        public void SetPickerPositionTopOn ()
-        {
-            this._rectMainNumberFrame = new RectangleF
-                (
-                    this._pStartPoint.X, 
-                    (this._pStartPoint.Y - this._fNumberPickerHeight), 
-                    this._fGlobalWidth, 
-                    (this._fNumberPickerHeight + this._fTxtNumberHeight)
-                );
-            this._rectNumberPicker = new RectangleF
-                (
-                    0.0f, 
-                    0.0f, 
-                    this._fGlobalWidth, 
-                    this._fNumberPickerHeight
-                );
-            this._rectTxtNumber = new RectangleF
-                (
-                    0.0f, 
-                    (this._fNumberPickerHeight), 
-                    this._fGlobalWidth,
-                    this._fTxtNumberHeight
-                );
-        }
-
-        public void SetPickerPositionBottomOn ()
-        {
-            this._rectNumberPicker = new RectangleF
-                (
-                    0.0f, 
-                    this._fMainNumberHeight, 
-                    this._fGlobalWidth, 
-                    this._fNumberPickerHeight
-                );
-            this._rectMainNumberFrame = new RectangleF
-                (
-                    this._pStartPoint.X, 
-                    this._pStartPoint.Y, 
-                    this._fGlobalWidth, 
-                    (this._fNumberPickerHeight + this._fMainNumberHeight)
-                );
-        }
-
-        public void SetPickerPositionNormalOff ()
-        {
-            this._rectMainNumberWithPicker = new RectangleF(
-                0.0f,
-                0.0f,
-                this._sLabelPickerViewSize.Width,
-                this._sLabelPickerViewSize.Height
-            );
-            this._rectMainNumberFrame = new RectangleF(
-                this._pStartPoint.X, 
-                this._pStartPoint.Y, 
-                this._fGlobalWidth, 
-                this._fTxtNumberHeight
-            );
-            this._rectTxtNumber = new RectangleF(
-                0.0f, 
-                0.0f, 
-                this._fGlobalWidth,
-                this._fTxtNumberHeight
-            );
-            this._rectUpButton = new RectangleF(
-                0.0f,
-                0.0f,
-                this._fGlobalWidth,
-                this._fUpDownButtonHeight
-            );
-            this._rectDownButton = new RectangleF(
-                0.0f,
-                this._fUpDownButtonHeight,
-                this._fGlobalWidth,
-                this._fUpDownButtonHeight
-            );
         }
 
         public void SetScale (int _scale)
@@ -270,79 +149,13 @@ namespace NathansWay.iOS.Numeracy.WorkSpace
 
         public void RefreshDisplay ()
         {
-            this.SetHeightWidth();
-            this.SetPickerPositionNormalOff();
+
         }
 
         #endregion
     }
 
-    // iOS dimensions
-    // Heights and widths of the initial number text box.
-    public struct iOSNumberDimensions
-    {
-        //this.AspyTag1 = 600102;
-        //this.AspyName = "VC_CtrlNumberText";
-        public float _sLabelPickerViewHeight;
-        public float _sLabelPickerViewWidth;
 
-        public float _fMainNumberHeight;
-        public float _fNumberPickerHeight;
-        public float _fTxtNumberHeight;
-        public float _fUpDownButtonHeight;
-        public float _fGlobalWidth;
-        // Label SizeF
-        //public SizeF _sLabelPickerViewSize;
-        // Font for textbox
-        public UIFont _globalFont;
-
-        public iOSNumberDimensions (G__NumberDisplaySize _size)
-        {
-            switch (_size)
-            {
-                case (G__NumberDisplaySize.Normal):
-                {
-                    this._sLabelPickerViewWidth = 130.0f;
-                    this._sLabelPickerViewHeight = 60.0f;
-
-                    this._fMainNumberHeight = 60.0f;
-                    this._fNumberPickerHeight = 180.0f;
-                    this._fTxtNumberHeight = 60.0f;
-                    this._fUpDownButtonHeight = 30.0f;
-                    this._fGlobalWidth = 46.0f;
-                    this._globalFont = UIFont.FromName("Arial", 55.0f);
-                }
-                break;
-                case (G__NumberDisplaySize.Medium):
-                {
-                    this._sLabelPickerViewWidth = 195.0f;
-                    this._sLabelPickerViewHeight = 60.0f;
-
-                    this._fMainNumberHeight = 90.0f;
-                    this._fNumberPickerHeight = 180.0f; // Stays the same
-                    this._fTxtNumberHeight = 90.0f;
-                    this._fUpDownButtonHeight = 45.0f;
-                    this._fGlobalWidth = 69.0f;
-                    this._globalFont = UIFont.FromName("Arial", 77.5f);
-                }
-                break;
-                case (G__NumberDisplaySize.Large):
-                {
-                    this._sLabelPickerViewWidth = 260.0f;
-                    this._sLabelPickerViewHeight = 60.0f;
-
-                    this._fMainNumberHeight = 120.0f;
-                    this._fNumberPickerHeight = 360.0f;
-                    this._fTxtNumberHeight = 120.0f;
-                    this._fUpDownButtonHeight = 60.0f;
-                    this._fGlobalWidth = 102.0f;
-                    this._globalFont = UIFont.FromName("Arial", 110.0f);
-                }
-                break;
-            }
-            //this._sLabelPickerViewSize = new SizeF(this._sLabelPickerViewWidth, this._sLabelPickerViewHeight);
-        }
-    }
 
 }
 
